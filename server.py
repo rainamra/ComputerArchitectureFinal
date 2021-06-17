9ade507a4c508180cb81aae5b79924fd93f427a7
rainamra <raina_mira@yahoo.com>
2021-06-17 20:12:21 +0700

    Second commit (client side)

--- server.py
+++ server.py
@@ -1,37 +1,35 @@
 import socket
-from _thread import (start_new_thread)
-import select
+from _thread import *
 
-HEADER_LENGTH = 10
+HEADER_LENGTH = 2048
 THREAD_COUNT = 0
 HOST = "127.0.0.1"
 PORT = 1234
 
 server_socket = socket.socket()
 
-
+print("Waiting to connect")
 try:
     server_socket.bind((HOST, PORT))
 except socket.error as err:
     print(str(err))
-
-print("Waiting to connect")
 server_socket.listen(5)
 
-def client_thread(connection):
-    connection.send(str.encode("Welcome to the server"))
+def client_thread(client, connection):
+    client.send(str.encode("Welcome to the server"))
+    print("The new connection was made from IP: " + connection[0] + " and port: " + str(connection[1]))
     while True:
-        data = connection.recv(HEADER_LENGTH)
-        reply = "Hello! I am server" + data.decode("utf-8")
+        data = client.recv(HEADER_LENGTH)
+        print("The client said: " + data.decode())
+        reply = data.decode("utf-8")
         if not data:
             break
-        connection.sendall(str.encode(reply))
-    connection.close()
+        client.sendall(str.encode(reply))
+    client.close()
 
 while True:
     client, address = server_socket.accept()
-    print("Connected to" + address[0] + str(address[1]))
-    start_new_thread(client_thread, (client,))
+    start_new_thread(client_thread, (client, address))
     THREAD_COUNT+=1
     print("Running Thread: " + str(THREAD_COUNT))
 server_socket.close()
