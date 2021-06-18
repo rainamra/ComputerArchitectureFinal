20249b279f0d0e5ce2078c93ec6ef7d18827d682
rainamra <raina_mira@yahoo.com>
2021-06-18 10:20:23 +0700

    UI still has error

--- client.py
+++ client.py
@@ -1,24 +1,90 @@
 import socket
+import threading
+from tkinter import Tk, Label, Text, Button, simpledialog, scrolledtext
+# import tkinter.scrolledtext
+import faulthandler; faulthandler.enable()
 
-client_socket = socket.socket()
-
-HEADER_LENGTH = 1024
-HOST = "127.0.0.1"
+HOST = '127.0.0.1'
 PORT = 1234
 
-print("Waiting to connect")
-try:
-    client_socket.connect((HOST, PORT))
-except socket.error as err:
-    print(str(err))
+class Client:
 
-Response = client_socket.recv(HEADER_LENGTH)
-print(Response.decode("utf-8"))
-input_name = input("What's your name? ")
+    def __init__(self, host, port):
+        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.client_socket.connect((host, port))
 
-while True:
-    input_text = input(input_name+ ": ")
-    client_socket.send(str.encode(input_name + ": "+ input_text))
-    response = client_socket.recv(HEADER_LENGTH)
-    print(response.decode("utf-8"))
-client_socket.close()
+        msg = Tk()
+        msg.withdraw()
+
+        self.username = simpledialog.askstring("Username", "Username:", parent=msg)
+
+        self.complete_interface = False
+        self.running = True
+
+        interface_thread = threading.Thread(target=self.client_interface)
+        receive_thread = threading.Thread(target=self.receive)
+
+        interface_thread.start()
+        receive_thread.start()
+
+    def client_interface(self):
+        self.win = Tk()
+        self.win.configure(bg="lightgray")
+
+        self.chat_label = Label(self.win, text="Chat: ", bg="lightgray")
+        self.chat_label.config(font=("Arial", 12))
+        self.chat_label.pack(padx=20, pady=5)
+
+        self.text_area = scrolledtext.ScrolledText(self.win)
+        self.text_area.pack(padx=20, pady=5)
+        self.text_area.config(state='disabled')
+
+        self.message_label = Label(self.win, text="Message: ", bg="lightgray")
+        self.message_label.config(font=("Arial", 12))
+        self.message_label.pack(padx=20, pady=5)
+
+        self.input_area = Text(self.win, height=3)
+        self.input_area.pack(padx=20, pady=5)
+
+        self.send_button = Button(self.win, text="Send", command=self.write)
+        self.send_button.config(font=("Arial", 12))
+        self.send_button.pack(padx=20, pady=5)
+
+        self.complete_interface = True
+
+        self.win.protocol("WM_DELETE_WINDOW", self.stop)
+
+        self.win.mainloop()
+
+
+    def write(self):
+        message = f"{self.username}: {self.input_area.get('1.0', 'end')}"
+        self.client_socket.send(message.encode('utf-8'))
+        self.input_area.delete('1.0', 'end')
+
+    def stop(self):
+        self.running = False
+        self.win.destroy()
+        self.client_socket.close()
+        exit(0)
+
+    def receive(self):
+        while self.running:
+            try:
+                message = self.client_socket.recv(1024)
+                if message.decode() == 'USERNAME':
+                    self.client_socket.send(self.username.encode('utf-8'))
+                else:
+                    if self.complete_interface:
+                        self.text_area.config(state='normal')
+                        self.text_area.insert('end', message)
+                        self.text_area.yview('end')
+                        self.text_area.config(state='disabled')
+            except ConnectionAbortedError:
+                break
+            except:
+                print('Error')
+                self.client_socket.close()
+                break
+
+client = Client(HOST, PORT)
--- server.py
+++ server.py
@@ -1,35 +1,55 @@
 import socket
-from _thread import *
+import threading
+import faulthandler; faulthandler.enable()
 
-HEADER_LENGTH = 2048
-THREAD_COUNT = 0
-HOST = "127.0.0.1"
+HOST = '127.0.0.1'
 PORT = 1234
 
-server_socket = socket.socket()
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.bind((HOST, PORT))
+server_socket.listen()
 
-print("Waiting to connect")
-try:
-    server_socket.bind((HOST, PORT))
-except socket.error as err:
-    print(str(err))
-server_socket.listen(5)
+clients = []
+usernames = []
 
-def client_thread(client, connection):
-    client.send(str.encode("Welcome to the server"))
-    print("The new connection was made from IP: " + connection[0] + " and port: " + str(connection[1]))
+def broadcast(message):
+    for client in clients:
+        client.send(message)
+
+def handle(client):
     while True:
-        data = client.recv(HEADER_LENGTH)
-        print("The client said: " + data.decode())
-        reply = data.decode("utf-8")
-        if not data:
+        try:
+            message = client.recv(1024)
+            print(f"{usernames[clients.index(client)]} says {message.decode()}")
+            broadcast(message)
+        except:
+            index = clients.index(client)
+            clients.remove(client)
+            client.close()
+            username = usernames[index]
+            broadcast(f"{username} has left the chat room!".encode('utf-8'))
+            usernames.remove(username)
             break
-        client.sendall(str.encode(reply))
-    client.close()
 
-while True:
-    client, address = server_socket.accept()
-    start_new_thread(client_thread, (client, address))
-    THREAD_COUNT+=1
-    print("Running Thread: " + str(THREAD_COUNT))
-server_socket.close()
+def receive():
+        while True:
+            print("Server is running and listening ...")
+            client, address = server_socket.accept()
+            print(f"Connected with {str(address)}")
+
+            client.send("USERNAME".encode('utf-8'))
+            username = client.recv(1024)
+
+            usernames.append(username)
+            clients.append(client)
+
+            print(f"The username of this client is {username}")
+            broadcast(f"New user has connected to the chat room. Say hi to {username}!".encode('utf-8'))
+            client.send("you are now connected!".encode('utf-8'))
+
+            thread = threading.Thread(target=handle, args=(client,))
+            thread.start()
+
+
+receive()
+
